807d1ec3b1e32f75be5264839df5b77174166b6f
StdEnku <SyplessEleki@gmail.com>
2023-02-24 11:51:34 +0900

    EnkuToolkitをバージョンアップ

--- 09.MainNavigationWindowNavigationService/Views/MainWindow.xaml.cs
+++ 09.MainNavigationWindowNavigationService/Views/MainWindow.xaml.cs
@@ -1,22 +1,12 @@
 ﻿namespace _09.MainNavigationWindowNavigationService.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using EnkuToolkit.Wpf.Controls;
 
 /// <summary>
 /// MainWindow.xaml の相互作用ロジック
 /// </summary>
-public partial class MainWindow : Window
+public partial class MainWindow : AnimatedNavigationWindow
 {
     public MainWindow()
     {
